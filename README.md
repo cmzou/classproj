e76570a19e433b4166c0a4881d4b82368d2c6ed2
jmz15 <39781351+cmzou@users.noreply.github.com>
2020-05-01 07:49:53 -0500

    add report

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# classproj
+# Classifying Court Documents by Violations
+
+Mortgage enforcement actions can provide insight into the efficacy of policy, enforcement trends, and prevalence of law breaking. Traditional methods to extracting information from texts such as the specific violations of the law can be costly. We explore the efficacy of machine learning models such as support vector machines and neural networks to aid in classification. We find that these methods can give high accuracy, but other metrics such as precision reveal they lack the ability to effectively find true positives.
+
+This repository hosts the code used to generate the results, the code for initial EDA, and the final report. 
--- eda.R
+++ eda.R
@@ -12,6 +12,23 @@ text <- text %>%
 reasons <- reasons %>% 
   mutate_all(as.character)
 
+# Remove labels
+toremove <- c('Unknown', 
+            'Other', 
+            'Did not appear for the Hearing', 
+            'Other failure to disclose', 
+            'Unknown failure to disclose', 
+            'Allowed falsification and misrepresentation of loans',
+            'Circumvented the requirements that a branch manager of a licensed mortgage broker have at least three years experience',
+            "Did not verify or make a reasonable effort to verify the borrower's information",
+            'Employed simultaneously by more than one affiliated mortgage banker or mortgage broker',
+            'Engaged in fraud',
+            'Failure to disclose charges',
+            'Violated NC Securities Act',
+            'Withdrew appeal',
+            'Unsatisfactory credit')
+reasons <- reasons[!reasons$reason %in% toremove,]
+  
 # Filter for docs in both csvs
 bothdocs <- intersect(text$docket_num, reasons$docket_num)
 text <- text %>% 
@@ -22,7 +39,7 @@ reasons <- reasons %>%
 # How important is multi-label classification?
 num_rperdoc <- table(reasons$docket_num)
 hist(num_rperdoc)
-# Although a majority have only one label, over 30 documents have two labels, 
+# Although a majority have only one label, 25 documents have two labels, 
 # and 10+ documents have three or more.
 # In a dataset this small, we should try multi-label classification.
 # Especially since our assumption is that a doc
